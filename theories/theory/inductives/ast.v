518472a133f8c2d8edb2e348c0d791af7ff3f4df
Gaetan Gilbert <gaetan.gilbert@ens-lyon.fr>
2017-03-24 16:14:29 +0100

    isembedding_{pair,exist} and adding existE

--- theories/theory/inductives/ast.v
+++ theories/theory/inductives/ast.v
@@ -31,7 +31,9 @@ Inductive exprS {T} (Γ : ctxS T) : (T -> Type) -> Type :=
 | constfunE : forall A B (f : A -> B), exprS Γ (fun _ => A) -> exprS Γ (fun _ => B)
 | varE : forall A, varS Γ A -> exprS Γ A
 | pairE : forall A B, exprS Γ A -> exprS Γ B -> exprS Γ (fun e => A e * B e)
-.
+| existE : forall A (B : forall e, A e -> Type) (x : exprS Γ A),
+    @exprS _ (consS T Γ A) (fun e => B e.1 e.2) ->
+    exprS Γ (fun e => sig (fun x : A e => B e x)).
 
 Fixpoint eval_expr {T} {Γ : ctxS T} {A} (a : exprS Γ A) e : A e
   := match a with
@@ -39,6 +41,8 @@ Fixpoint eval_expr {T} {Γ : ctxS T} {A} (a : exprS Γ A) e : A e
      | constfunE A B f a => f (eval_expr a e)
      | varE A a => eval_var a e
      | pairE A B a b => (eval_expr a e, eval_expr b e)
+     | existE A B a b =>
+       (eval_expr a e; eval_expr b (e;_))
      end.
 
 Fixpoint uses_embeddings {T Γ A} (a : @exprS T Γ A) : Type :=
@@ -47,6 +51,7 @@ Fixpoint uses_embeddings {T Γ A} (a : @exprS T Γ A) : Type :=
   | constfunE A B f a => IsEmbedding f * uses_embeddings a
   | varE _ _ => Unit
   | pairE A B a b => uses_embeddings a * uses_embeddings b
+  | existE A B a b => uses_embeddings a * uses_embeddings b
   end.
 
 Fixpoint ishprop_uses_embeddings `{Funext} {T Γ A} (a : @exprS T Γ A) : IsHProp (uses_embeddings a)
@@ -55,5 +60,63 @@ Fixpoint ishprop_uses_embeddings `{Funext} {T Γ A} (a : @exprS T Γ A) : IsHPro
      | constfunE A B f a => trunc_prod
      | varE _ _ => trunc_succ
      | pairE A B a b => trunc_prod
+     | existE A B a b => trunc_prod
      end.
 Existing Instance ishprop_uses_embeddings.
+
+Instance isembedding_pair {A B C D} (f : A -> B) (g : C -> D)
+         {fembed : IsEmbedding f} {gembed : IsEmbedding g}
+  : IsEmbedding (fun x => (f (fst x), g (snd x))).
+Proof.
+  intros [y1 y2].
+  unfold hfiber.
+  simple refine (trunc_equiv' ((hfiber f y1) * (hfiber g y2)) _).
+  srefine (BuildEquiv _ _ _ (isequiv_adjointify _ _ _ _)).
+  - intros p. exists ((fst p).1, (snd p).1). simpl.
+    apply path_prod';[exact ((fst p).2)|exact ((snd p).2)].
+  - intros p. split.
+    + exists (fst p.1). exact (ap fst p.2).
+    + exists (snd p.1). exact (ap snd p.2).
+  - intros [[x1 x2] p]. simpl.
+    apply ap. apply eta_path_prod.
+  - intros x;simpl.
+    apply path_prod;simpl.
+    + set (lhs := _ : sig _). apply (path_sigma _ lhs (fst x) idpath).
+      simpl;clear lhs.
+      apply (@ap_fst_path_prod _ _ (_,_) (_,_)).
+    + set (lhs := _ : sig _). apply (path_sigma _ lhs (snd x) idpath).
+      simpl;clear lhs.
+      apply (@ap_snd_path_prod _ _ (_,_) (_,_)).
+Defined.
+
+Instance isembedding_exist {A B C D} (f : A -> B) (g : forall x, C x -> D (f x))
+         {fembed : IsEmbedding f} {gembed : forall x, IsEmbedding (g x)}
+  : IsEmbedding (fun x => (f x.1;g x.1 x.2)).
+Proof.
+  intros [y1 y2].
+  unfold hfiber.
+  srefine (trunc_equiv' (sig (fun p : hfiber f y1 => hfiber (g p.1) (transport _ p.2^ y2))) _).
+  srefine (equiv_adjointify _ _ _ _).
+  - intros p. exists (p.1.1;p.2.1).
+    simpl. apply (path_sigma' _ p.1.2).
+    apply moveR_transport_p.
+    exact p.2.2.
+  - intros p. srefine ((p.1.1;_);(p.1.2;_));simpl.
+    + exact (pr1_path p.2).
+    + apply moveL_transport_V. exact (pr2_path p.2).
+  - intros [[x1 x2] p];simpl in *. apply ap.
+    revert p;apply (equiv_ind (path_sigma_uncurried _ _ _)).
+    simpl;intros [p1 p2].
+    destruct p1,p2;simpl. reflexivity.
+  - intros [[x1 p1] [x2 p2]];simpl in *.
+    destruct p1;simpl in p2. destruct p2;reflexivity.
+Defined.
+
+Instance isembedding_dup_hset {A} {Aset : IsHSet A} : IsEmbedding (fun x : A => (x,x)).
+Proof.
+  intros [y1 y2].
+  unfold hfiber;simpl. apply ishprop_sigma_disjoint.
+  intros x1 x2 p1 p2.
+  apply (ap fst) in p1;apply (ap fst) in p2;simpl in p1, p2.
+  path_via y1.
+Defined.
